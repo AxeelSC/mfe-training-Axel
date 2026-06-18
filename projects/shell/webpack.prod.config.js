3de29ef606edb1a1dcd563a2b53d5066b21e4139
AxelSC <axelsc.dev@gmail.com>
2026-06-18 11:37:03 +0200

    Configure shell production remotes

--- projects/shell/webpack.prod.config.js
+++ projects/shell/webpack.prod.config.js
@@ -1,1 +1,17 @@
-module.exports = require('./webpack.config');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require('@angular-architects/module-federation/webpack');
+
+const remoteBaseUrl = 'https://axeelsc.github.io/mfe-training-Axel';
+
+module.exports = withModuleFederationPlugin({
+  remotes: {
+    catalog: `${remoteBaseUrl}/catalog/remoteEntry.js`,
+    reports: `${remoteBaseUrl}/reports/remoteEntry.js`,
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+});
